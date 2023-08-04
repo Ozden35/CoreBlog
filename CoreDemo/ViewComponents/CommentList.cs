7c16ebea34377e3aafd09754173db397291b26d1
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-04 21:00:26 +0300

    -Bloglara ait yorumların listelenmesi, view component ve parametre kullanımı sweet alert kullanımı

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Identity;
+using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentvalues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID=1,
+					  Username = "Eğlaz"
+				},
+				   new UserComment
+				  {
+					ID=2,
+					Username="Yaprak"
+				  },
+					new UserComment
+				  {
+					ID=3,
+					Username="Sunay"
+				  }
+			 };
+			return View(commentvalues);
+		}
+	}
+}
