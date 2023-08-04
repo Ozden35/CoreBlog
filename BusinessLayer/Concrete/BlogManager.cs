17b6cfbbfeabab392aa28474243fa047c54317c9
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-05 02:57:46 +0300

    Login panelinin hazırlanması, yazar blog ilişkisinin kurulması, mail bültenine abone olma ve bloglara yorum yapma.

--- BusinessLayer/Concrete/BlogManager.cs
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -52,5 +52,10 @@ namespace BusinessLayer.Concrete
 		{
 			return _blogDal.GetListAll();
 		}
+
+		public List<Blog> GetBlogListByWriter(int id)
+		{
+			return _blogDal.GetListAll(X => X.WriterID == id);
+		}
 	}
 }
