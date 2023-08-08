224514480d30f8b51f09e161ce33393d7764e26a
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-08 16:38:16 +0300

    Navbar düzenlemeleri, yazar üst meni view components, yazarın kendine ait blog sitesi ve yazarın yeni blog eklemesi

--- BusinessLayer/Abstract/IGenericService.cs
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -7,9 +7,12 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-	public interface IBlogService : IGenericService<Blog>
-	{
-		List<Blog> GetBlogListWithCategory();
-		List<Blog> GetBlogListByWriter(int id);
-	}
+    public interface IGenericService<T>
+    {
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> GetList();
+        T GetById(int id);
+    }
 }
