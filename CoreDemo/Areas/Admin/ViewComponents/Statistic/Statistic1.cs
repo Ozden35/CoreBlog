93dc4143afd39fd57a4b9c0c300807432af31606
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-13 21:24:45 +0300

    Api üzerinden hava durumu bilgisi çekme ve Google chart1

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,11 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "8918c532560bb36048e912bfb5885939";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
