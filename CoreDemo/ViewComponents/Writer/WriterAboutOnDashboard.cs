cf811d0a2bdb8aa716db42977bc98f7080deb128
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-18 23:27:03 +0300

    Identity İle Giriş Yapan Kullanıcının Bilgilerini Getirme

--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.ViewComponents.Writer
@@ -8,11 +10,14 @@ namespace CoreDemo.ViewComponents.Writer
     public class WriterAboutOnDashboard : ViewComponent
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
+
         Context c = new Context();
 
-        public IViewComponentResult Invoke()
+		public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
+            var username = User.Identity.Name;
+            ViewBag.veri = username;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = wm.GetWriterByID(writerID);
             return View(values);
