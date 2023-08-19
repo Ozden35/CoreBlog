de8da6cace6dffe0e753e17a051a6a3f194fc221
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-20 01:55:39 +0300

    Identity FindByNameAsync ve UpdateAsync ve Identity ile Şifre Güncelleme

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,20 +1,21 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
-using DataAccessLayer.EntityFramework;
-using EntityLayer.Concrete;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using FluentValidation.Results;
 using CoreDemo.Models;
 using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
-namespace CoreDemo.Controllers
+namespace CoreBlog.Controllers
 {
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
         UserManager userManager = new UserManager(new EfUserRepository());
+        Context c = new Context();
 
         private readonly UserManager<AppUser> _userManager;
 
@@ -28,9 +29,8 @@ namespace CoreDemo.Controllers
         {
             var usermail = User.Identity.Name;
             ViewBag.v = usermail;
-            Context c = new Context();
-            var writerName = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterName).FirstOrDefault();
-            ViewBag.v2 = writerName;
+            var writername = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterName).FirstOrDefault();
+            ViewBag.v2 = writername;
             return View();
         }
         public IActionResult WriterProfile()
@@ -41,48 +41,55 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
-        [AllowAnonymous]
         public IActionResult Test()
         {
             return View();
         }
-        [AllowAnonymous]
         public PartialViewResult WriterNavbarPartial()
         {
             return PartialView();
         }
-        [AllowAnonymous]
-        public  PartialViewResult WriterFooterPartial()
+
+        public PartialViewResult WriterFooterPartial()
         {
             return PartialView();
         }
-        [HttpGet]
-        public IActionResult WriterEditProfile()
-        {
-            Context c = new Context();
-            var username = User.Identity.Name;
-            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
-            //var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
-            //var writervalues = wm.TGetById(writerID);
-            //return View(writervalues);
-            //var username = await _userManager.FindByNameAsync(User.Identity.Name);
-            var id = c.Users.Where(x => x.Email == usermail).Select(y => y.Id).FirstOrDefault();
-            var values = userManager.TGetById(id);
-            return View(values);
-        }
 
-        [HttpPost]
-        public IActionResult WriterEditProfile(Writer p)
+
+        [HttpGet]
+        public async Task<IActionResult> WriterEditProfile()
         {
-            wm.TUpdate(p);
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserUpdateViewModel model = new UserUpdateViewModel();
+            model.mail = values.Email;
+            model.namesurname = values.NameSurname;
+            model.imageurl = values.ImageUrl;
+            model.username = values.UserName;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> WriterEditProfile(UserUpdateViewModel model)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            values.NameSurname = model.namesurname;
+            values.ImageUrl = model.imageurl;
+            values.Email = model.mail;
+            values.PasswordHash = _userManager.PasswordHasher.HashPassword(values, model.password);
+            var result = await _userManager.UpdateAsync(values);
             return RedirectToAction("Index", "Dashboard");
         }
-        [AllowAnonymous]
+
+
         [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+        [HttpPost]
         public IActionResult WriterAdd(AddProfileImage p)
         {
             Writer w = new Writer();
-            if(p.WriterImage != null)
+            if (p.WriterImage != null)
             {
                 var extension = Path.GetExtension(p.WriterImage.FileName);
                 var newimagename = Guid.NewGuid() + extension;
