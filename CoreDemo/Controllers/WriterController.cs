4c05445d7241404c84e12700d88b08c899c72e3b
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-19 03:05:21 +0300

    Yazar Bilgilerinin Identitye Bağlanması

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -7,12 +7,22 @@ using Microsoft.AspNetCore.Mvc;
 using FluentValidation.Results;
 using CoreDemo.Models;
 using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 
 namespace CoreDemo.Controllers
 {
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
+        UserManager userManager = new UserManager(new EfUserRepository());
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public WriterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
@@ -52,9 +62,13 @@ namespace CoreDemo.Controllers
             Context c = new Context();
             var username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
-            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
-            var writervalues = wm.TGetById(writerID);
-            return View(writervalues);
+            //var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            //var writervalues = wm.TGetById(writerID);
+            //return View(writervalues);
+            //var username = await _userManager.FindByNameAsync(User.Identity.Name);
+            var id = c.Users.Where(x => x.Email == usermail).Select(y => y.Id).FirstOrDefault();
+            var values = userManager.TGetById(id);
+            return View(values);
         }
 
         [HttpPost]
