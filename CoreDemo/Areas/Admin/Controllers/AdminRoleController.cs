455b6d21a784da8638a801abd10ca8505b47b675
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-08-22 21:03:38 +0300

    Rol Düzenleme ve Rol Silme

--- CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
@@ -1,4 +1,5 @@
-﻿using CoreDemo.Models;
+﻿using BusinessLayer.Concrete;
+using CoreDemo.Models;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -49,6 +50,39 @@ namespace CoreDemo.Areas.Admin.Controllers
             }
             return View(model);
         }
+        [HttpGet]
+        public IActionResult UpdateRole(int id)
+        {
+            var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            RoleUpdateViewModel model = new RoleUpdateViewModel
+            {
+                id=values.Id,
+                name = values.Name
+            };
 
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(RoleUpdateViewModel model)
+        {
+            var values = _roleManager.Roles.Where(x => x.Id == model.id).FirstOrDefault();
+            values.Name = model.name;
+            var result = await _roleManager.UpdateAsync(values);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            var result = await _roleManager.DeleteAsync(values);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
